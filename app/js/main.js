4ca16c5ecee2e4334ed8b27673d9bf8aff0e1896
AndTaran <65190142+AndTaran@users.noreply.github.com>
2021-01-03 20:03:40 +0300

    feat: executed header and three blocks

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,14 @@
 // $(function () {
 //     $('body').css('background-color', 'red')
 // })
+
+$(function () {
+
+  $('.top__slider').slick({
+    // infinite: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  });
+
+})
