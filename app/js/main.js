3dd55ef13a4af18cc1b6c77f3ba0038aea88cae1
AndTaran <65190142+AndTaran@users.noreply.github.com>
2021-01-06 16:49:53 +0300

    feat: add burger menu and responsiveness

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,3 @@
-// $(function () {
-//     $('body').css('background-color', 'red')
-// })
 
 $(function () {
 
@@ -11,13 +8,22 @@ $(function () {
     autoplaySpeed: 3000,
   });
 
-  $(window).scroll(function(){
-    if($(this).scrollTop()>123){
-        $('.menu').addClass('fixed');
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 123 && window.innerWidth > 800) {
+      $('.menu').addClass('fixed');
+    } else if ($(this).scrollTop() < 125 && window.innerWidth > 800) {
+      $('.menu').removeClass('fixed');
     }
-    else if ($(this).scrollTop()<125){
-        $('.menu').removeClass('fixed');
-    }
-});
+  });
+
+  $('.menu__btn').on('click', function () {
+    $('.menu__btn').toggleClass('menu__btn--active');
+    $('.menu').toggleClass("menu--active");
+  });
+
+  // $('.menu__list-link').on('click', function () {
+  //   $('.menu__btn').removeClass('menu__btn--active');
+  //   $('.menu__list').removeClass('menu__list--active');
+  // });
 
 })
